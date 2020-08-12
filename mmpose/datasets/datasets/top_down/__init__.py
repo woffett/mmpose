f0bab9d18dd83db23327dffc90781b5742bd2a52
jin-s13 <jinsheng13@foxmail.com>
2020-08-12 18:22:29 +0800

    add TopDownAicDataset

--- mmpose/datasets/datasets/top_down/__init__.py
+++ mmpose/datasets/datasets/top_down/__init__.py
@@ -1,4 +1,5 @@
+from .topdown_aic_dataset import TopDownAicDataset
 from .topdown_coco_dataset import TopDownCocoDataset
 from .topdown_mpii_trb_dataset import TopDownMpiiTrbDataset
 
-__all__ = ['TopDownCocoDataset', 'TopDownMpiiTrbDataset']
+__all__ = ['TopDownCocoDataset', 'TopDownMpiiTrbDataset', 'TopDownAicDataset']
