3dc2e0d202b0606884272c7d52521c85adcb5268
Jonathan Li <jlli@google.com>
2020-08-17 05:26:12 +0000

    expose one more utility function in inference API
    Signed-off-by: Jonathan Li <jlli@google.com>

--- mmpose/apis/__init__.py
+++ mmpose/apis/__init__.py
@@ -1,8 +1,9 @@
-from .inference import Compose, LoadImage, _box2cs, inference_pose_model, init_pose_model, vis_pose_result
+from .inference import Compose, LoadImage, _box2cs, _xyxy2xywh, inference_pose_model, init_pose_model, vis_pose_result
 from .test import multi_gpu_test, single_gpu_test
 from .train import train_model
 
 __all__ = [
     'train_model', 'init_pose_model', 'inference_pose_model', 'multi_gpu_test',
-    'single_gpu_test', 'vis_pose_result', 'Compose', 'LoadImage', '_box2cs'
+    'single_gpu_test', 'vis_pose_result', 'Compose', 'LoadImage', '_box2cs',
+    '_xyxy2xywh'
 ]
