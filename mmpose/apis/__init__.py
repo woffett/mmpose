cf8cbf49759e745896b70ce69d412518568af33b
Jonathan Li <jlli@google.com>
2020-08-17 06:11:19 +0000

    expose coordinate conversion utility function in api
    Signed-off-by: Jonathan Li <jlli@google.com>

--- mmpose/apis/__init__.py
+++ mmpose/apis/__init__.py
@@ -1,9 +1,9 @@
-from .inference import Compose, LoadImage, _box2cs, _xyxy2xywh, inference_pose_model, init_pose_model, vis_pose_result
+from .inference import Compose, LoadImage, _box2cs, _xyxy2xywh, _xywh2xyxy, inference_pose_model, init_pose_model, vis_pose_result
 from .test import multi_gpu_test, single_gpu_test
 from .train import train_model
 
 __all__ = [
     'train_model', 'init_pose_model', 'inference_pose_model', 'multi_gpu_test',
     'single_gpu_test', 'vis_pose_result', 'Compose', 'LoadImage', '_box2cs',
-    '_xyxy2xywh'
+    '_xyxy2xywh', '_xywh2xyxy'
 ]
